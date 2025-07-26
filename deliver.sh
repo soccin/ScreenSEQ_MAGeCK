d8903552ef7d13f5e2815450f32356c3accdbf33
Nicholas D. Socci <soccin@mskcc.org>
2025-07-26 12:48:24 -0400

    Better fix

--- deliver.sh
+++ deliver.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-DDIR=$1
+DIR=$1
 
 if [ "$#" -ne 1 ]; then
     echo -e "\n   usage: deliver.sh //pi/user/Proj_no/r_00x\n"
     exit
 fi
 
-mkdir -p $DDIR/sgrna/mageck
-rsync -avP --exclude-from="EXC" counts $DDIR/sgna/mageck
-rsync -avP --exclude-from="EXC" out $DDIR/sgna/mageck
+DDIR=$DIR/sgrna/mageck
+mkdir -p $DDIR
+rsync -avP --exclude-from="EXC" counts $DDIR
+rsync -avP --exclude-from="EXC" out $DDIR
 
