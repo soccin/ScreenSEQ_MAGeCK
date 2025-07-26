576e6b4aaaef3d6ed73e209b6210a591b8231807
Nicholas D. Socci <soccin@mskcc.org>
2025-07-26 12:21:17 -0400

    Fix paths and missing files

--- testMAGeCK.sh
+++ testMAGeCK.sh
@@ -3,6 +3,8 @@
 TARGET=$1
 CONTROL=$2
 
+COUNTS_FILE=$(ls counts/*.count.txt)
+
 TARGET_SAMPLES=$(cat key.csv | egrep ",${TARGET}$" | cut -f1 -d, | xargs | tr ' ' ',')
 CONTROL_SAMPLES=$(cat key.csv | egrep ",${CONTROL}$" | cut -f1 -d, | xargs | tr ' ' ',')
 
@@ -12,10 +14,12 @@ echo "-c $CONTROL_SAMPLES"
 ODIR=out/${TARGET},${CONTROL}
 mkdir -p $ODIR
 
+. ve.mageck/bin/activate
+
 mageck test \
-     -k counts/p17150.count.txt \
+     -k $COUNTS_FILE \
      -t $TARGET_SAMPLES \
      -c $CONTROL_SAMPLES \
      -n $ODIR/${TARGET}_vs_${CONTROL} \
-     --control-sgrna controlProbes.txt \
+     --control-sgrna dat/controlProbes.txt \
      --pdf-report
