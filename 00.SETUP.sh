ac04452fd522815136f1b55e0ca3728412325e10
Nicholas D. Socci <soccin@mskcc.org>
2025-07-26 01:17:11 -0400

    Create and use src directory and load new gcc

--- 00.SETUP.sh
+++ 00.SETUP.sh
@@ -1,4 +1,6 @@
-wget https://sourceforge.net/projects/mageck/files/0.5/mageck-0.5.9.5.tar.gz/download -O mageck-0.5.9.5.tar.gz
+module load gcc/11.2.0
+mkdir src
+wget https://sourceforge.net/projects/mageck/files/0.5/mageck-0.5.9.5.tar.gz/download -O src/mageck-0.5.9.5.tar.gz
 tar xvfz src/mageck-0.5.9.5.tar.gz
 
 /juno/work/bic/socci/opt/common/CentOS_7/python/python-3.11.3/bin/python3 -m venv ve.mageck
